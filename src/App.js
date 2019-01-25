cdf209fa8b767ef7534d4834eed93dd1c48a459d
Jorge Mancia <mancia.a.jorge@gmail.com>
2019-01-25 14:19:12 -0800

    Add husky

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+const Warning = React.lazy(() => import('./Warning'))
+
+class App extends React.Component {
+  state = {
+    count: 0
+  }
+
+  render() {
+    const { count } = this.state
+    return (
+      <div>
+        <h1>Hello, World!!</h1>
+        <img alt="company logo" src="logo.jpg" />
+        <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
+
+        <button
+          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
+        >
+          +
+        </button>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
+        >
+          -
+        </button>
+        {count > 10 ? (
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+        ) : null}
+      </div>
+    )
+  }
+}
+
+export default hot(module)(App)
